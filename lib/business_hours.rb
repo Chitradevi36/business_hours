e94ce497a07f0af20b23dcff639ffc547d0da68c
Chitradevi36 <chitra.rajaguru123@gmail.com>
2022-09-04 12:45:38 +0530

    Initial Greetings added

---
+++ lib/business_hours.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "business_hours/version"
+
+module BusinessHours
+  def self.greetings(name)
+    puts "Hi #{name}!"
+  end
+end
---
+++ lib/business_hours/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BusinessHours
+  VERSION = "0.1.0"
+end
